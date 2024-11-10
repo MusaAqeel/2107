e382e9586752837bc44e54f069fcea71b483ba40
Gira Chawda <64598936+girachawda@users.noreply.github.com>
2024-11-10 13:22:41 -0500

    Frontend Pages (#18)
    * Installing Jest Testing Software
    
    * Add FED tests with Jest for home and feedback page
    
    * FED pages

---
+++ app/LLM/LLM.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const LLM = (submittedInput: String, submittedPlaylistLength: number) => {
+    return (
+        <>
+            <h1>Mock</h1>
+        </>
+    )
+};
+
+export default LLM;
