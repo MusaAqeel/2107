eccc16bb1c15317ddbb406014b9e509541e90f0b
Toby Carter <148788610+Toby-J-Carter@users.noreply.github.com>
2025-03-09 21:19:23 -0400

    Fix footer themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default async function RootLayout({
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
